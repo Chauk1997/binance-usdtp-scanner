4eafa88d650ef41e8ec7bc3b085096554186131f
Chauk1997 <jimmy0423go@gmail.com>
2026-09-19 18:03:59 +0800

    Fix 4h cache init route

--- main.py
+++ main.py
@@ -505,7 +505,8 @@ async def cache_init_1d():
             for x in one_d
         ),
         "rate_limited": RATE_LIMITED,
-    }@app.get("/cache/init/4h")
+    }
+    @app.get("/cache/init/4h")
 async def cache_init_4h():
     global RATE_LIMITED
 
